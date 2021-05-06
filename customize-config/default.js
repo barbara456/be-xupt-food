77e4b7a4dd747e35fb4fd0b6841e196a2d2fab39
gaojiahui <289300388@qq.com>
2021-05-06 20:04:18 +0800

    customize-config

---
+++ customize-config/default.js
@@ -0,0 +1,12 @@
+const config = {
+    port: 8888,
+    database: {
+        DATABASE: '',
+        USERNAME: '',
+        PASSWORD: '',
+        PORT: '',
+        HOST: ''
+    }
+}
+
+module.exports = config
